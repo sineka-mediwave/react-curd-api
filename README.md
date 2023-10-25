76e74d723b11b1ffde86c24323584c80d8c5c8eb
sineka-mediwave <sineka@mindwaveventures.com>
2023-10-25 16:50:03 +0530

    router

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 - create your react app
 - install these packages
   `npm install react-router-dom axios bootstrap json-server`
+- Create pages to route (Create, Home, Read, Update)
+- route the pages with app
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+import Update from "./pages/Update";
+import Read from "./pages/Read";
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/create" element={<Create />}></Route>
+        <Route path="/update/:id" element={<Update />}></Route>
+        <Route path="/read/:id" element={<Read />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
